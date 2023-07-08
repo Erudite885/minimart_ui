b78983258758a83f55dcf36b4e13abacc7af138d
erudite885 <uchebianca@gmail.com>
2023-07-08 09:22:25 +0100

    initialization of main files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Footer, Navbar } from "./components/index"
+
+const App = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Footer } from "./footer/Footer";
+export { default as Featured } from "./featured/Featured";
+export { default as TrustedBy } from "./trustedBy/TrustedBy";
+export { default as Slide } from "./slide/Slide";
+export { default as CatCard } from "./catcard/CatCard";
+export { default as ProjectCard } from "./projectcard/ProjectCard";
+export { default as GigCard } from "./gigcard/GigCard";
