03e2e6490ea52442d7efca931d28dc086c4688fe
erudite885 <uchebianca@gmail.com>
2023-07-10 22:05:37 +0100

    feat/components: footer component and styling fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React from "react";
+
+import { ReactIcon } from "../../assets";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span> Graphic & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span> Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span> Graphic & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span> Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span> Graphic & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span> Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span> Graphic & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span> Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span> Graphic & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span> Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>minimart</h2>
+            <span>c minimart international Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src={ReactIcon} alt="" />
+              <img src={ReactIcon} alt="" />
+              <img src={ReactIcon} alt="" />
+              <img src={ReactIcon} alt="" />
+              <img src={ReactIcon} alt="" />
+            </div>
+            <div className="link">
+              <img src={ReactIcon} alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src={ReactIcon} alt="" />
+              <span>USD</span>
+            </div>
+            <img src={ReactIcon} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
