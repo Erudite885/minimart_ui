b1a2aeba7db6edc0f5ef26efad08c4aa0dd85fa4
erudite885 <uchebianca@gmail.com>
2023-07-10 16:05:45 +0100

    feat/components: slide component added to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Home.scss";
-import { Featured, TrustedBy } from "../../components/index";
+import { CatCard, Featured, Slide, TrustedBy } from "../../components/index";
+import { cards } from "../../data";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
+      </Slide>
     </div>
   );
 };
