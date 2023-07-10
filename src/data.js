f3d206a8063f19ebf0c399c48ea9ddc7102583f3
erudite885 <uchebianca@gmail.com>
2023-07-10 16:04:30 +0100

    feat/data: cards dummy added

---
+++ src/data.js
@@ -0,0 +1,76 @@
+import { ReactIcon } from "./assets";
+
+export const cards = [
+  {
+    id: 1,
+    title: "AI Artists",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 2,
+    title: "AI Brand",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 3,
+    title: "AI Design",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 4,
+    title: "AI Press",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 5,
+    title: "AI Word",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 6,
+    title: "AI Book ",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 7,
+    title: "AI Covers",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 8,
+    title: "AI Voice",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 9,
+    title: "AI Over ",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 10,
+    title: "AI Chat",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 11,
+    title: "AI Transact",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+  {
+    id: 12,
+    title: "AI Finance",
+    desc: "lorem ipsum",
+    img: ReactIcon,
+  },
+];
