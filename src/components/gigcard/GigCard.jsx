31917a805e6145ea5a5616795b99223aaf610837
erudite885 <uchebianca@gmail.com>
2023-07-10 22:38:24 +0100

    feat/components: gigscard component and styling fixed

--- src/components/gigcard/GigCard.jsx
+++ src/components/gigcard/GigCard.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactIcon } from "../../assets";
+import "./GigCard.scss";
 
-const GigCard = () => {
+const GigCard = ({ item }) => {
   return (
-    <div>GigCard</div>
-  )
-}
+    <Link to="/gig/123">
+      <div className="gigCard">
+        <img src={item.img} alt="" />
+        <div className="info">
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <img src={ReactIcon} alt="" />
+            <span>{item.star}</span>
+          </div>
+        </div>
+        <hr />
+        <div className="details">
+          <img src={ReactIcon} alt="" />
+          <div className="price">
+            <span>STARTING AT</span>
+            <h2>${item.price}</h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default GigCard
+export default GigCard;
