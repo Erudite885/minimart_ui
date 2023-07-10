7519952dc832881db2f6748cad906485b3f660eb
erudite885 <uchebianca@gmail.com>
2023-07-10 14:59:17 +0100

    feat/pages: featured imported to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Home.scss";
+import { Featured } from "../../components/index";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
