482af5dbbab0c21a4d287507821121241fe34f15
erudite885 <uchebianca@gmail.com>
2023-07-10 15:16:40 +0100

    feat/components: trustedby component & styling fixed

--- src/components/trustedBy/TrustedBy.jsx
+++ src/components/trustedBy/TrustedBy.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import { ReactIcon } from "../../assets";
+import "./TrustedBy.scss";
 
 const TrustedBy = () => {
   return (
-    <div>TrustedBy</div>
-  )
-}
+    <div className="trustedBy">
+      <div className="container">
+        <span>Trusted by: </span>
+        <img src={ReactIcon} alt="" />
+        <img src={ReactIcon} alt="" />
+        <img src={ReactIcon} alt="" />
+        <img src={ReactIcon} alt="" />
+        <img src={ReactIcon} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default TrustedBy
+export default TrustedBy;
