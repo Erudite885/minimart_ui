21b4065f75133e4588edba6568e5283819a57462
erudite885 <uchebianca@gmail.com>
2023-07-10 13:29:02 +0100

    app router set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 import React from "react";
-import { Footer, Navbar } from "./components/index"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import {
+  Add,
+  Gig,
+  Gigs,
+  Home,
+  Message,
+  Messages,
+  MyGigs,
+  Orders,
+} from "./pages/index";
+import { Footer, Navbar } from "./components/index";
+
+import "./App.scss";
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/gigs", element: <Gigs /> },
+        { path: "/gig/:id", element: <Gig /> },
+        { path: "/orders", element: <Orders /> },
+        { path: "/myGigs", element: <MyGigs /> },
+        { path: "/add", element: <Add /> },
+        { path: "/messages", element: <Messages /> },
+        { path: "/message/:id", element: <Message /> },
+      ],
+    },
+  ]);
+
   return (
     <div className="">
-      <Navbar />
-      {/* <Footer /> */}
+      <RouterProvider router={router} />
     </div>
   );
 };
