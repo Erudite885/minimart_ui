8283d88d0849862af27f458f813bb01c7c95ccfb
erudite885 <uchebianca@gmail.com>
2023-07-17 01:10:35 +0100

    slide & message fixed

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,34 +1,20 @@
 import React from "react";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 import { ReactIcon } from "../../assets";
-=======
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
 import "./Message.scss";
-import { Link } from "react-router-dom";
+
 
 const Message = () => {
   return (
     <div className="message">
       <div className="container">
         <span className="breadcrumbs">
-<<<<<<< HEAD
           <Link to="/messages"> MESSAGES </Link> > JoHN DOE >
-=======
-          {/* <Link to="/messages"> */}
-            MESSAGES
-          {/* </Link> */}
-           > JoHN DOE >
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
         </span>
         <div className="messages">
           <div className="item">
             <img
-<<<<<<< HEAD
               src={ReactIcon}
-=======
-              // src="https://fiverr-res.cloundinary.com/1_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
               alt=""
             />
             <p>
@@ -40,11 +26,7 @@ const Message = () => {
           </div>
           <div className="item owner">
             <img
-<<<<<<< HEAD
               src={ReactIcon}
-=======
-              // src="https://fiverr-res.cloundinary.com/1_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
               alt=""
             />
             <p>
@@ -56,11 +38,7 @@ const Message = () => {
           </div>
           <div className="item">
             <img
-<<<<<<< HEAD
               src={ReactIcon}
-=======
-              // src="https://fiverr-res.cloundinary.com/1_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
               alt=""
             />
             <p>
@@ -72,11 +50,7 @@ const Message = () => {
           </div>
           <div className="item owner">
             <img
-<<<<<<< HEAD
               src={ReactIcon}
-=======
-              // src="https://fiverr-res.cloundinary.com/1_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
               alt=""
             />
             <p>
@@ -88,11 +62,7 @@ const Message = () => {
           </div>
           <div className="item">
             <img
-<<<<<<< HEAD
               src={ReactIcon}
-=======
-              // src="https://fiverr-res.cloundinary.com/1_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
               alt=""
             />
             <p>
@@ -104,11 +74,7 @@ const Message = () => {
           </div>
           <div className="item owner">
             <img
-<<<<<<< HEAD
               src={ReactIcon}
-=======
-              // src="https://fiverr-res.cloundinary.com/1_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png"
->>>>>>> 5f14f295cfe1ad71c6e4aedea656aa089c4f863e
               alt=""
             />
             <p>
